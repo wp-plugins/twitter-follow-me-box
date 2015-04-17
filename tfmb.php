d00c41b17b637092d39f3b9731204f70ab013d89
lcrafael <lcrafael@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-17 17:21:38 +0000

    add fix urls
    git-svn-id: https://plugins.svn.wordpress.org/twitter-follow-me-box/trunk@1137378 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- tfmb.php
+++ tfmb.php
@@ -1,11 +1,11 @@
 <?php
 /*
 Plugin Name: Twitter Follow Me Box
-Plugin URI: http://www.soumae.org/twitter-follow-box/
+Plugin URI: http://www.cirolini.com.br/wordpress-plugin-twiiter-follow-me-box/
 Description: Creates a box "follow me" twitter without touching the template
 Version: 2.1
 Author: Rafael Cirolini
-Author URI: http://www.soumae.org/
+Author URI: http://www.cirolini.com.br/
 License: GPL2
 */
 
